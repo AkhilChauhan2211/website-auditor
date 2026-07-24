e5a8377062beea9cd83551d1b24d8b0cfcc77b2d
AkhilChauhan2211 <chauhanakhil573@gmaiul.com>
2026-07-24 22:39:24 +0530

    Complete frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://website-auditor-production-36bc.up.railway.app/api/audit";
 
 export const auditWebsite = (url) => {
-    return axios.post(`${API_URL}/audit`, {
+    return axios.post(`${API_URL}`, {
         url: url
     });
 };
